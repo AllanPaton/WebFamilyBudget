a6ffb7f691ba66d938fe91a018e168284dbe0f21
AllanPaton <milkshtorm@gmail.com>
2024-04-14 19:07:08 +0500

    Delete unusable and parse base

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
 				      <div className="list-fixed"> {/*  Список с функциональными кнопками  */}
 					      <div>1</div>
 					      <div>2</div>
+						  <hr/>
 					      <div>3</div>
 					      <div>Search</div>
 				      </div>
@@ -18,26 +19,53 @@ function App() {
 				      <div className="main-app-container">
 					      <div className="app-header">
 						      <div className="flex-bar">
-							      <div>Income</div>
-							      <div>Outcome</div>
-							      <div>Balance</div>
+							      <div>Income: 9999</div>
+							      <div>Outcome: 9999</div>
+							      <div>Balance: 9999</div>
 						      </div>
 					      </div>
 					      <div className="main-app-content">
-						      <div className="flexbox-container">
-							      <div className="main-app-leftSlice">
+								  <div className="flexbox-container">
+									  <div className="main-app-leftSlice">
+										  <div className="boldWin">
+											  <button>PREV</button>
+											  <button>THIS</button>
+											  <button>NEXT</button>
+											  <div>
+												  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+											  </div>
+										  </div>
+										  <div className="boldWin">
+											  <button>PREV</button>
+											  <button>THIS</button>
+											  <button>NEXT</button>
+											  <div>
+												  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+													  eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+													  enim ad minim veniam, quis nostrud exercitation ullamco laboris
+													  nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+													  reprehenderit in voluptate velit esse cillum dolore eu fugiat
+													  nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+													  sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
-							      </div>
-							      <div className="main-app-rightSlice">
-
-							      </div>
-						      </div>
-					      </div>
-				      </div>
-			      </div>
-		      </div>
-	      </div>
-      </div>
+											  </div>
+										  </div>
+									  </div>
+								  </div>
+							  <div className="main-app-rightSlice">
+								  <div>List</div>
+								  <div>
+									  <div>
+										  COntent
+									  </div>
+								  </div>
+							  </div>
+						  </div>
+					  </div>
+				  </div>
+			  </div>
+		  </div>
+	  </div>
   );
 }
 
